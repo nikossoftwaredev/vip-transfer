b965cfcc5b373d2d3888cf9a7d55c8e3d4bc4e13
Nikos Dimitrakopoulos <ndimitrakopoulos@i4.energy>
2024-02-20 14:05:59 +0200

    fixed card dollar

--- components/TourCard.tsx
+++ components/TourCard.tsx
@@ -7,11 +7,15 @@ export const TourCard = ({ cost, hours, image, subtitle, title }: TourConfig) =>
     <div className="card bg-base-300 max-w-[400px] cursor-pointer hover:scale-105 transition ease-in-out">
       <section className="relative">
         <img src={image} className="max-h-[250px] h-[250px] w-full rounded-xl border-b-0" />
-        <span className="flex items-center gap-1 absolute top-1 right-1"><Chip icon={<MdAccessTime />} title={hours.toString()} /><Chip icon={'$'} title={`${cost}`} color="text-green-600" /> </span>
+        <span className="flex items-center gap-1 absolute top-1 right-1"><Chip icon={<MdAccessTime />} title={hours.toString()} /> </span>
       </section>
-      <div className="card-body">
-        <h2 className="card-title">{title}</h2>
+      <div className="px-8 py-4">
+        <h2 className="card-title mb-2">{title}</h2>
         <p>{subtitle}</p>
+        <div className="flex w-full mt-2">
+          <span className="text-green-500 ml-auto">{cost} $</span>
+
+        </div>
 
       </div>
     </div>
